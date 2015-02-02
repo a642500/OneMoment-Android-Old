4e0ca2c2692d2f98dbe263ec3b7603f61a4024e3
Carlos <toxz@foxmail.com>
2015-02-02 23:07:56 +0800

    create base project.

---
+++ app/src/co/yishun/onemoment/app/MainActivity.java
@@ -0,0 +1,15 @@
+package co.yishun.onemoment.app;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Carlos on 2/2/15.
+ */
+public class MainActivity extends Activity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
