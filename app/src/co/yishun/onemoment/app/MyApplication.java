4501a936d1e1e7d79af732635daf5f83e8846665
Carlos <toxz@foxmail.com>
2015-02-26 18:01:34 +0800

    Code:add some Helpers provide basic dependency methods. add Application component.

---
+++ app/src/co/yishun/onemoment/app/MyApplication.java
@@ -0,0 +1,11 @@
+package co.yishun.onemoment.app;
+
+import android.app.Application;
+import org.androidannotations.annotations.EApplication;
+
+/**
+ * Created by Carlos on 2/3/15.
+ */
+@EApplication
+public class MyApplication extends Application {
+}
