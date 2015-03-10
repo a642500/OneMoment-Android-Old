9e225a2f741ed5bbc52886a8ad41485826256e20
Carlos <toxz@foxmail.com>
2015-03-10 10:26:49 +0800

    add ORMlite provider lib finish some todo

--- app/src/co/yishun/onemoment/app/data/Moment.java
+++ app/src/co/yishun/onemoment/app/data/Moment.java
@@ -2,14 +2,15 @@ package co.yishun.onemoment.app.data;
 
 import com.j256.ormlite.field.DatabaseField;
 import com.j256.ormlite.table.DatabaseTable;
+import com.tojc.ormlite.android.annotation.AdditionalAnnotation;
 
 /**
  * This bean is used in Ormlite and OrmliteProvider.
  * <p/>
  * Created by Carlos on 2/13/15.
  */
-//TODO add DefaultContentUri(authority = Contract.AUTHORITY, path=Contract.Moment.CONTENT_URI_PATH)
-//TODO add DefaultContentMimeTypeVnd(name=Contract.Moment.MIMETYPE_NAME, type=Contract.Moment.MIMETYPE_TYPE)
+@AdditionalAnnotation.DefaultContentUri(authority = Contract.AUTHORITY, path = Contract.Moment.CONTENT_URI_PATH)
+@AdditionalAnnotation.DefaultContentMimeTypeVnd(name = Contract.Moment.MIMETYPE_NAME, type = Contract.Moment.MIMETYPE_TYPE)
 @DatabaseTable(tableName = Contract.DATABASE_NAME)
 public class Moment {
     @DatabaseField(columnName = Contract.Moment._ID, generatedId = true)
