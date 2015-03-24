800c11eaa9b979622ea9390d7ac79bb17e3833dd
Carlos <toxz@foxmail.com>
2015-03-24 16:57:09 +0800

    add login entry

--- app/src/main/java/co/yishun/onemoment/app/ui/AlbumActivity.java
+++ app/src/main/java/co/yishun/onemoment/app/ui/AlbumActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Pair;
+import android.view.ContextMenu;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -60,10 +61,12 @@ public class AlbumActivity extends ActionBarActivity {
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
 
+        switch (item.getItemId()) {
+            case R.id.action_login:
+                LoginActivity_.intent(this).start();
+                break;
+        }
         return super.onOptionsItemSelected(item);
     }
 
