c28ce7ab9fee7117b2f0d27eaf8737c571dfdb88
Carlos <toxz@foxmail.com>
2015-03-24 16:32:47 +0800

    update login act

--- app/src/main/java/co/yishun/onemoment/app/ui/LoginActivity.java
+++ app/src/main/java/co/yishun/onemoment/app/ui/LoginActivity.java
@@ -1,7 +1,8 @@
 package co.yishun.onemoment.app.ui;
 
-import android.app.Activity;
 import android.support.annotation.NonNull;
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.widget.Toolbar;
 import android.text.Editable;
 import android.view.View;
 import android.widget.TextView;
@@ -13,70 +14,74 @@ import co.yishun.onemoment.app.net.result.AccountResult;
 import co.yishun.onemoment.app.net.result.VerificationResult;
 import co.yishun.onemoment.app.util.AccountHelper;
 import com.koushikdutta.async.future.FutureCallback;
-import org.androidannotations.annotations.AfterTextChange;
-import org.androidannotations.annotations.Click;
-import org.androidannotations.annotations.EActivity;
-import org.androidannotations.annotations.UiThread;
+import org.androidannotations.annotations.*;
 
 /**
  * Created by Carlos on 2/15/15.
  */
 @EActivity(R.layout.login_layout)
-public class LoginActivity extends Activity {
-private String mPhoneNum;
-private String mVerificationCode;
+public class LoginActivity extends ActionBarActivity {
+    private String mPhoneNum;
+    private String mVerificationCode;
 
-@AfterTextChange(R.id.phoneEditText) void onPhoneChange(Editable text, TextView phone) {
+    @AfterTextChange(R.id.phoneEditText)
+    void onPhoneChange(Editable text, TextView phone) {
         mPhoneNum = text.toString();
-        }
+    }
 
-@AfterTextChange(R.id.verificationEditText) void onVerificationCodeChange(Editable text, TextView phone) {
+    @AfterTextChange(R.id.verificationEditText)
+    void onVerificationCodeChange(Editable text, TextView phone) {
         mVerificationCode = text.toString();
-        }
+    }
 
-@Click(R.id.verificationBtn) void onVerification(@NonNull View verificationBtn) {
-        if (checkPhoneNum())
-        new PhoneVerification.SendSms().setPhone(String.valueOf(mPhoneNum)).setCallback(new FutureCallback<VerificationResult>() {
-@Override
-public void onCompleted(Exception e, VerificationResult result) {
 
-        }
-        });
-        }
+    @Click(R.id.loginBtn)
+    void onLogin(@NonNull View loginBtn) {
 
-@Click(R.id.loginBtn) void onLogin(@NonNull View loginBtn) {
+    }
 
-        }
-
-@Click(R.id.nextBtn) void onNext(@NonNull View nextBtn) {
+    @Click(R.id.nextBtn)
+    void onNext(@NonNull View nextBtn) {
         if (checkPhoneNum() && checkVerificationCode())
-        new PhoneVerification.Verify().setPhone(mPhoneNum).setVerifyCode(mVerificationCode).setCallback(new FutureCallback<VerificationResult>() {
-@Override public void onCompleted(Exception e, VerificationResult result) {
+            new PhoneVerification.Verify().setPhone(mPhoneNum).setVerifyCode(mVerificationCode).setCallback(new FutureCallback<VerificationResult>() {
+                @Override
+                public void onCompleted(Exception e, VerificationResult result) {
 
-        }
-        });
+                }
+            });
         else
-        Toast.makeText(this, "Your phone or verification code is wrong", Toast.LENGTH_SHORT).show();
-        }
+            Toast.makeText(this, "Your phone or verification code is wrong", Toast.LENGTH_SHORT).show();
+    }
+
+    @ViewById
+    Toolbar toolbar;
+
+    @AfterViews
+    void initToolbar() {
+        setSupportActionBar(toolbar);
+        toolbar.setTitle(getString(R.string.loginByPhoneTitle));
+
+    }
 
 
-@UiThread void signUpSuccess(AccountResult result) {
+    @UiThread
+    void signUpSuccess(AccountResult result) {
         switch (result.getCode()) {
-        case ErrorCode.SUCCESS:
+            case ErrorCode.SUCCESS:
 
-        break;
-default:
-        Toast.makeText(this, "success", Toast.LENGTH_LONG).show();
-        break;
+                break;
+            default:
+                Toast.makeText(this, "success", Toast.LENGTH_LONG).show();
+                break;
         }
-        }
+    }
 
-private boolean checkPhoneNum() {
+    private boolean checkPhoneNum() {
         return AccountHelper.isValidPhoneNum(mPhoneNum);
-        }
+    }
 
-private boolean checkVerificationCode() {
+    private boolean checkVerificationCode() {
         return AccountHelper.isValidVerificationCode(mVerificationCode);
-        }
+    }
 
-        }
+}
