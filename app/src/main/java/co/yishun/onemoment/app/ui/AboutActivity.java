2494f839ab4a88eda4f1423c05b9a398c8a68d20
Carlos <toxz@foxmail.com>
2015-03-26 17:47:24 +0800

    add AboutActivity

---
+++ app/src/main/java/co/yishun/onemoment/app/ui/AboutActivity.java
@@ -0,0 +1,27 @@
+package co.yishun.onemoment.app.ui;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import co.yishun.onemoment.app.R;
+import org.androidannotations.annotations.AfterViews;
+import org.androidannotations.annotations.EActivity;
+import org.androidannotations.annotations.ViewById;
+
+@EActivity(R.layout.activity_about)
+public class AboutActivity extends ActionBarActivity {
+
+    @ViewById
+    Toolbar toolbar;
+
+    @AfterViews
+    void initToolbar() {
+        setSupportActionBar(toolbar);
+        toolbar.setNavigationIcon(R.drawable.ic_back);
+        toolbar.setTitle(getString(R.string.aboutTitle));
+        toolbar.setNavigationOnClickListener(v -> AboutActivity.this.onBackPressed());
+    }
+}
