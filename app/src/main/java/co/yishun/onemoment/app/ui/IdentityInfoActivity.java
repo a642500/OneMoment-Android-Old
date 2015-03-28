035db36f67fba7b474697742acb3e90d30ceab3f
Carlos <toxz@foxmail.com>
2015-03-28 12:38:43 +0800

    create IdentityInfoActitivy

---
+++ app/src/main/java/co/yishun/onemoment/app/ui/IdentityInfoActivity.java
@@ -0,0 +1,13 @@
+package co.yishun.onemoment.app.ui;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import co.yishun.onemoment.app.R;
+import org.androidannotations.annotations.EActivity;
+
+@EActivity(R.layout.activity_identity_info)
+public class IdentityInfoActivity extends ActionBarActivity {
+
+}
