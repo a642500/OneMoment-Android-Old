a53e96550813cd74428fd02f687ed542c98c6be1
Carlos <toxz@foxmail.com>
2015-03-28 09:56:51 +0800

    update login dialog ui

--- app/src/main/java/co/yishun/onemoment/app/ui/AboutActivity.java
+++ app/src/main/java/co/yishun/onemoment/app/ui/AboutActivity.java
@@ -1,5 +1,6 @@
 package co.yishun.onemoment.app.ui;
 
+import android.content.Context;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
@@ -7,7 +8,9 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import co.yishun.onemoment.app.R;
+import com.afollestad.materialdialogs.MaterialDialog;
 import org.androidannotations.annotations.AfterViews;
+import org.androidannotations.annotations.Click;
 import org.androidannotations.annotations.EActivity;
 import org.androidannotations.annotations.ViewById;
 
@@ -23,5 +26,14 @@ public class AboutActivity extends ActionBarActivity {
         toolbar.setNavigationIcon(R.drawable.ic_back);
         toolbar.setTitle(getString(R.string.aboutTitle));
         toolbar.setNavigationOnClickListener(v -> AboutActivity.this.onBackPressed());
+    }
+
+    @Click(R.id.checkNewBtn)
+    void checkNew() {
+        showUpdateDialog(this);
+    }
+
+    public static void showUpdateDialog(Context context) {
+        new MaterialDialog.Builder(context).backgroundColorRes(R.color.bgUpdatedDialogColor).customView(R.layout.updated_dialog).show();
     }
 }
