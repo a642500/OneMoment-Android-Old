cc111fe5ca75637b72da28ca24b256eed2429e80
Carlos <toxz@foxmail.com>
2015-03-28 12:46:07 +0800

    add IdentityInfoActivity basic ui

--- app/src/main/java/co/yishun/onemoment/app/ui/IdentityInfoActivity.java
+++ app/src/main/java/co/yishun/onemoment/app/ui/IdentityInfoActivity.java
@@ -2,12 +2,29 @@ package co.yishun.onemoment.app.ui;
 
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import co.yishun.onemoment.app.R;
+import co.yishun.onemoment.app.util.LogUtil;
+import org.androidannotations.annotations.AfterViews;
 import org.androidannotations.annotations.EActivity;
+import org.androidannotations.annotations.ViewById;
 
 @EActivity(R.layout.activity_identity_info)
 public class IdentityInfoActivity extends ActionBarActivity {
+    private static final String TAG = LogUtil.makeTag(IdentityInfoActivity.class);
+
+    @ViewById
+    Toolbar toolbar;
+
+    @AfterViews
+    void initToolbar() {
+        setSupportActionBar(toolbar);
+        toolbar.setNavigationIcon(R.drawable.ic_back);
+        toolbar.setTitle(getString(R.string.identityInfoTitle));
+        toolbar.setNavigationOnClickListener(v -> IdentityInfoActivity.this.onBackPressed());
+    }
+
 
 }
