0ae62f8f7aaa6a7e5b4432ec0a8d9e2d28713833
Carlos <toxz@foxmail.com>
2015-03-31 12:37:18 +0800

    try to align album text ,failed

---
+++ app/src/main/java/co/yishun/onemoment/app/ui/view/FuckTextView.java
@@ -0,0 +1,151 @@
+package co.yishun.onemoment.app.ui.view;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.drawable.Drawable;
+import android.text.TextPaint;
+import android.util.AttributeSet;
+import android.view.View;
+import co.yishun.onemoment.app.R;
+import co.yishun.onemoment.app.util.LogUtil;
+
+/**
+ * TODO: document your custom view class.
+ */
+public class FuckTextView extends View {
+    private static final String TAG = LogUtil.makeTag(FuckTextView.class);
+    private String mText = "00"; // TODO: use a default from R.string...
+    private int mTextColor = Color.RED; // TODO: use a default from R.color...
+    private float mTextSize = 12; // TODO: use a default from R.dimen...
+
+    private TextPaint mTextPaint;
+    private float mTextWidth;
+    private float mTextHeight;
+
+    public FuckTextView(Context context) {
+        super(context);
+        init(null, 0);
+    }
+
+    public FuckTextView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        init(attrs, 0);
+    }
+
+    public FuckTextView(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+        init(attrs, defStyle);
+    }
+
+    private void init(AttributeSet attrs, int defStyle) {
+        // Load attributes
+        final TypedArray a = getContext().obtainStyledAttributes(attrs, R.styleable.FuckTextView, defStyle, 0);
+
+        mText = a.getString(R.styleable.FuckTextView_text);
+        mTextColor = a.getColor(R.styleable.FuckTextView_textColor, mTextColor);
+        // Use getDimensionPixelSize or getDimensionPixelOffset when dealing with
+        // values that should fall on pixel boundaries.
+        mTextSize = a.getDimension(R.styleable.FuckTextView_textSize, mTextSize);
+        a.recycle();
+
+        // Set up a default TextPaint object
+        mTextPaint = new TextPaint();
+        mTextPaint.setFlags(Paint.ANTI_ALIAS_FLAG);
+        mTextPaint.setTextAlign(Paint.Align.LEFT);
+
+        // Update TextPaint and text measurements from attributes
+        invalidateTextPaintAndMeasurements();
+    }
+
+    private void invalidateTextPaintAndMeasurements() {
+        mTextPaint.setTextSize(mTextSize);
+        mTextPaint.setColor(mTextColor);
+        mTextWidth = mTextPaint.measureText(mText);
+
+        Paint.FontMetrics fontMetrics = mTextPaint.getFontMetrics();
+        mTextHeight = fontMetrics.bottom;
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+
+        // Draw the text.
+        canvas.drawText(mText, mTextWidth / 2, mTextHeight / 2, mTextPaint);
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+        LogUtil.d(TAG, "w: " + widthMeasureSpec + "h: " + heightMeasureSpec);
+        invalidateTextPaintAndMeasurements();
+        LogUtil.d(TAG, "w: " + mTextWidth + "h: " + mTextHeight);
+        setMeasuredDimension(
+                MeasureSpec.makeMeasureSpec((int) mTextWidth, MeasureSpec.EXACTLY)
+                , MeasureSpec.makeMeasureSpec((int) mTextHeight, MeasureSpec.EXACTLY));
+    }
+
+
+    /**
+     * Gets the example string attribute value.
+     *
+     * @return The example string attribute value.
+     */
+    public String getText() {
+        return mText;
+    }
+
+    /**
+     * Sets the view's example string attribute value. In the example view, this string
+     * is the text to draw.
+     *
+     * @param exampleString The example string attribute value to use.
+     */
+    public void setText(String exampleString) {
+        mText = exampleString;
+        invalidateTextPaintAndMeasurements();
+    }
+
+    /**
+     * Gets the example color attribute value.
+     *
+     * @return The example color attribute value.
+     */
+    public int getColor() {
+        return mTextColor;
+    }
+
+    /**
+     * Sets the view's example color attribute value. In the example view, this color
+     * is the font color.
+     *
+     * @param exampleColor The example color attribute value to use.
+     */
+    public void setColor(int exampleColor) {
+        mTextColor = exampleColor;
+        invalidateTextPaintAndMeasurements();
+    }
+
+    /**
+     * Gets the example dimension attribute value.
+     *
+     * @return The example dimension attribute value.
+     */
+    public float getDimension() {
+        return mTextSize;
+    }
+
+    /**
+     * Sets the view's example dimension attribute value. In the example view, this dimension
+     * is the font size.
+     *
+     * @param exampleDimension The example dimension attribute value to use.
+     */
+    public void setDimension(float exampleDimension) {
+        mTextSize = exampleDimension;
+        invalidateTextPaintAndMeasurements();
+    }
+}
