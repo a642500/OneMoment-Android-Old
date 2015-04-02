a23406d1187207bd8a92877316c2e442720b5f56
Carlos <toxz@foxmail.com>
2015-04-02 11:36:55 +0800

    add IntegrateInfoActivity

---
+++ app/src/main/java/co/yishun/onemoment/app/ui/IntegrateInfoActivity.java
@@ -0,0 +1,87 @@
+package co.yishun.onemoment.app.ui;
+
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import co.yishun.onemoment.app.R;
+import co.yishun.onemoment.app.util.LogUtil;
+import com.afollestad.materialdialogs.MaterialDialog;
+import com.afollestad.materialdialogs.Theme;
+import org.androidannotations.annotations.AfterViews;
+import org.androidannotations.annotations.Click;
+import org.androidannotations.annotations.EActivity;
+import org.androidannotations.annotations.ViewById;
+
+/**
+ * Created by Carlos on 2015/4/2.
+ */
+@EActivity(R.layout.activity_integrate_info)
+public class IntegrateInfoActivity extends ActionBarActivity {
+    public static final String TAG = LogUtil.makeTag(IntegrateInfoActivity.class);
+
+    @ViewById
+    Toolbar toolbar;
+
+    @AfterViews
+    void initToolbar() {
+        setSupportActionBar(toolbar);
+        toolbar.setNavigationIcon(R.drawable.ic_back);
+        toolbar.setTitle(getString(R.string.integrateInfoTitle));
+        toolbar.setNavigationOnClickListener(v -> IntegrateInfoActivity.this.onBackPressed());
+    }
+
+    @ViewById
+    EditText nameEditText;
+
+
+    @Click
+    void areaItemClicked(View view) {
+
+    }
+
+
+    public static final int MALE = 0;
+    public static final int FEMALE = 1;
+
+    @ViewById
+    TextView genderTextView;
+
+    private void setGender(int gender) {
+        switch (gender) {
+            case MALE:
+                genderTextView.setText('♂');
+                break;
+            case FEMALE:
+                genderTextView.setText('♀');
+                break;
+            default:
+                LogUtil.e(TAG, "unknown gender!!");
+                break;
+        }
+    }
+
+    @Click
+    void genderItemClicked(View view) {
+        new MaterialDialog.Builder(this)
+                .theme(Theme.DARK)
+                .title(R.string.integrateInfoGenderHint)
+                .items(R.array.integrateInfoGenderArray)
+                .itemsCallbackSingleChoice(MALE, new MaterialDialog.ListCallbackSingleChoice() {
+                    @Override
+                    public boolean onSelection(MaterialDialog dialog, View view, int which, CharSequence text) {
+                        setGender(which);
+                        return true; // allow selection
+                    }
+                })
+                .positiveText(R.string.integrateInfoChooseBtn)
+                .show();
+    }
+
+    @Click
+    void okBtnClicked(View view) {
+
+    }
+
+}
