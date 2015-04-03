2c74479fb3ca7d76e2b133a18199fdc131d80231
Carlos <toxz@foxmail.com>
2015-04-03 16:30:05 +0800

    add share entry

--- app/src/main/java/co/yishun/onemoment/app/ui/AboutActivity.java
+++ app/src/main/java/co/yishun/onemoment/app/ui/AboutActivity.java
@@ -1,6 +1,8 @@
 package co.yishun.onemoment.app.ui;
 
 import android.content.Context;
+import android.content.Intent;
+import android.view.View;
 import co.yishun.onemoment.app.R;
 import com.afollestad.materialdialogs.MaterialDialog;
 import org.androidannotations.annotations.Click;
@@ -23,5 +25,10 @@ public class AboutActivity extends ToolbarBaseActivity {
         return new MaterialDialog.Builder(context).customView(R.layout.dialog_check, false).backgroundColorRes(R.color.bgUpdatedDialogColor).show();
     }
 
+    @Click
+    void recommendBtnClicked(View view) {
+        Intent sendIntent = new Intent().setAction(Intent.ACTION_SEND).putExtra(Intent.EXTRA_TEXT, getString(R.string.aboutRecommendContent)).setType("text/plain");
+        startActivity(sendIntent);
+    }
 
 }
