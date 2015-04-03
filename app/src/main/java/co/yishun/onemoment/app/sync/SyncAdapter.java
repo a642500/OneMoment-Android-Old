4f23fc0a252056d74d034c3f4737c39e9cd589cc
Carlos <toxz@foxmail.com>
2015-04-03 17:56:20 +0800

    log when syncing

--- app/src/main/java/co/yishun/onemoment/app/sync/SyncAdapter.java
+++ app/src/main/java/co/yishun/onemoment/app/sync/SyncAdapter.java
@@ -3,14 +3,16 @@ package co.yishun.onemoment.app.sync;
 import android.accounts.Account;
 import android.content.*;
 import android.os.Bundle;
+import co.yishun.onemoment.app.util.LogUtil;
 
 /**
  * Handle the transfer of data between a server and an
  * app, using the Android sync adapter framework.
- * <p/>
+ * <p>
  * Created by Carlos on 3/10/15.
  */
 public class SyncAdapter extends AbstractThreadedSyncAdapter {
+    private static final String TAG = LogUtil.makeTag(SyncAdapter.class);
     ContentResolver mContentResolver;
 
     public SyncAdapter(Context context, boolean autoInitialize) {
@@ -26,6 +28,7 @@ public class SyncAdapter extends AbstractThreadedSyncAdapter {
     @Override
     public void onPerformSync(Account account, Bundle extras, String authority, ContentProviderClient provider, SyncResult syncResult) {
         //TODO transfer data to server
+        LogUtil.i(TAG, "onPerformSync, account: " + account.name);
         //see:http://developer.android.com/training/sync-adapters/creating-sync-adapter.html
     }
 }
