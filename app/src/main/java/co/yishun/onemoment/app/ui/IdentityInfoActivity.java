5f32f5c5b2fc45313468877300212333a4e5bd9c
Carlos <toxz@foxmail.com>
2015-04-03 12:30:23 +0800

    make IdentityInfo partly work

--- app/src/main/java/co/yishun/onemoment/app/ui/IdentityInfoActivity.java
+++ app/src/main/java/co/yishun/onemoment/app/ui/IdentityInfoActivity.java
@@ -10,18 +10,17 @@ import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 import co.yishun.onemoment.app.R;
+import co.yishun.onemoment.app.net.request.account.IdentityInfo;
 import co.yishun.onemoment.app.net.result.AccountResult;
 import co.yishun.onemoment.app.util.AccountHelper;
 import co.yishun.onemoment.app.util.LogUtil;
 import com.squareup.picasso.Picasso;
-import org.androidannotations.annotations.AfterViews;
-import org.androidannotations.annotations.Click;
-import org.androidannotations.annotations.EActivity;
-import org.androidannotations.annotations.ViewById;
+import org.androidannotations.annotations.*;
 
 @EActivity(R.layout.activity_identity_info)
 public class IdentityInfoActivity extends ToolbarBaseActivity {
     private static final String TAG = LogUtil.makeTag(IdentityInfoActivity.class);
+    public static final int REQUEST_UPDATE_INFO = 0;
 
     @ViewById
     ImageView profileImageView;
@@ -39,28 +38,38 @@ public class IdentityInfoActivity extends ToolbarBaseActivity {
         AccountResult.Data data = AccountHelper.getIdentityInfo(this);
         Picasso.with(this).load(data.getAvatar_url()).into(profileImageView);
         nickNameTextView.setText(data.getNickname());
-        weiboTextView.setText(data.getWeibo_uid());
+        weiboTextView.setText(data.getWeibo_uid());//TODO set text
+        setGender(data.getGender());
+        areaTextView.setText(data.getArea());
+    }
+
+    private void setGender(String gender) {
+        if (gender.startsWith("m"))
+            genderTextView.setText(String.valueOf('\u2642'));
+        else if (gender.startsWith("f"))
+            genderTextView.setText(String.valueOf('\u2640'));
+        else
+            genderTextView.setText(getString(R.string.integrateInfoGenderPrivate));
     }
 
     @Click({R.id.profileItem, R.id.nickNameItem, R.id.weiboItem, R.id.genderItem, R.id.areaItem})
     void infoItem(View view) {
         Toast.makeText(this, "click", Toast.LENGTH_SHORT).show();
         switch (view.getId()) {
-            case R.id.profileItem:
-
-                break;
-            case R.id.nickNameItem:
-                break;
             case R.id.weiboItem:
-                break;
-            case R.id.genderItem:
-                break;
-            case R.id.areaItem:
-                break;
-            case R.id.logoutBtn:
+                //TODO bind weibo
                 break;
             default:
+                //TODO put extras to let it preset
+                IdentityInfoActivity_.intent(this).startForResult(REQUEST_UPDATE_INFO);
                 break;
+        }
+    }
+
+    @OnActivityResult(REQUEST_UPDATE_INFO)
+    void onUpdate(int resultCode) {
+        if (resultCode == RESULT_OK) {
+            initViews();
         }
     }
 
