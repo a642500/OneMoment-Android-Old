39e49b4318f3abd7bad890d817395c075b388fdb
Carlos <toxz@foxmail.com>
2015-04-05 18:52:43 +0800

    use http get

--- app/src/main/java/co/yishun/onemoment/app/net/request/sync/GetToken.java
+++ app/src/main/java/co/yishun/onemoment/app/net/request/sync/GetToken.java
@@ -6,9 +6,11 @@ import co.yishun.onemoment.app.util.DecodeUtil;
 import co.yishun.onemoment.app.util.LogUtil;
 import com.google.gson.Gson;
 import com.koushikdutta.async.future.FutureCallback;
+import com.squareup.okhttp.OkHttpClient;
+import com.squareup.okhttp.Response;
 
+import java.io.IOException;
 import java.io.Serializable;
-import java.util.concurrent.ExecutionException;
 
 /**
  * Created by Carlos on 2015/4/5.
@@ -32,14 +34,15 @@ public class GetToken extends Request<GetToken.TokenResult> {
         check();
         if (builder != null && callback != null) {
             try {
-                builder.load(getUrl())
-                        .setBodyParameter("key", key)
-                        .setBodyParameter("filename", fileName)
-                        .asString()
-                        .setCallback((e, result) ->
-                                callback.onCompleted(e, new Gson().fromJson(DecodeUtil.decode(result), TokenResult.class))).get();
-            } catch (InterruptedException | ExecutionException e) {
-                e.printStackTrace();
+                OkHttpClient client = new OkHttpClient();
+                com.squareup.okhttp.Request request = new com.squareup.okhttp.Request.Builder().url(getUrl() + "?key=" + key + "&filename=" + fileName)
+                        .get().build();
+                Response response = client.newCall(request).execute();
+
+                callback.onCompleted(null, new Gson().fromJson(DecodeUtil.decode(response.body().string()), TokenResult.class));
+//    GET /api/v2/upload_token?key=<key>&filename=<filename>
+            } catch (IOException e) {
+                callback.onCompleted(e, null);
             }
         }
     }
--- app/src/main/java/co/yishun/onemoment/app/net/request/sync/GetVideoList.java
+++ app/src/main/java/co/yishun/onemoment/app/net/request/sync/GetVideoList.java
@@ -6,9 +6,11 @@ import co.yishun.onemoment.app.util.AccountHelper;
 import co.yishun.onemoment.app.util.DecodeUtil;
 import com.google.gson.Gson;
 import com.koushikdutta.async.future.FutureCallback;
+import com.squareup.okhttp.OkHttpClient;
+import com.squareup.okhttp.Response;
 
+import java.io.IOException;
 import java.io.Serializable;
-import java.util.concurrent.ExecutionException;
 
 /**
  * Created by Carlos on 2015/4/5.
@@ -25,13 +27,14 @@ public class GetVideoList extends Request<GetVideoList.VideoListResult> {
         check();
         if (builder != null && callback != null) {
             try {
-                builder.load(getUrl())
-                        .setBodyParameter("key", key)
-                        .asString()
-                        .setCallback((e, result) ->
-                                callback.onCompleted(e, new Gson().fromJson(DecodeUtil.decode(result), VideoListResult.class))).get();
-            } catch (InterruptedException | ExecutionException e) {
-                e.printStackTrace();
+                OkHttpClient client = new OkHttpClient();
+                com.squareup.okhttp.Request request = new com.squareup.okhttp.Request.Builder().url(getUrl() + "?key=" + key)
+                        .get().build();
+                Response response = client.newCall(request).execute();
+
+                callback.onCompleted(null, new Gson().fromJson(DecodeUtil.decode(response.body().string()), VideoListResult.class));
+            } catch (IOException e) {
+                callback.onCompleted(e, null);
             }
         }
     }
