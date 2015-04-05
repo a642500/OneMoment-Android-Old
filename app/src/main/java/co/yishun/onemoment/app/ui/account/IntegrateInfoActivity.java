dfaca5c21a6ce48b4a88bc9dc87e2b709c369ea5
Carlos <toxz@foxmail.com>
2015-04-06 00:37:50 +0800

    add function code

--- app/src/main/java/co/yishun/onemoment/app/ui/account/IntegrateInfoActivity.java
+++ app/src/main/java/co/yishun/onemoment/app/ui/account/IntegrateInfoActivity.java
@@ -1,13 +1,18 @@
 package co.yishun.onemoment.app.ui.account;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.text.TextUtils;
 import android.view.View;
 import android.widget.*;
 import co.yishun.onemoment.app.R;
+import co.yishun.onemoment.app.config.Config;
 import co.yishun.onemoment.app.config.Constants;
 import co.yishun.onemoment.app.config.ErrorCode;
 import co.yishun.onemoment.app.net.request.account.IdentityInfo;
+import co.yishun.onemoment.app.net.request.sync.GetToken;
 import co.yishun.onemoment.app.ui.ToolbarBaseActivity;
 import co.yishun.onemoment.app.util.AccountHelper;
 import co.yishun.onemoment.app.util.LogUtil;
@@ -15,8 +20,13 @@ import com.afollestad.materialdialogs.MaterialDialog;
 import com.afollestad.materialdialogs.Theme;
 import com.daimajia.androidanimations.library.Techniques;
 import com.daimajia.androidanimations.library.YoYo;
+import com.qiniu.android.storage.UploadManager;
+import com.soundcloud.android.crop.Crop;
+import com.squareup.picasso.Picasso;
 import org.androidannotations.annotations.*;
 import org.androidannotations.annotations.res.StringArrayRes;
+
+import java.io.File;
 
 /**
  * Created by Carlos on 2015/4/2.
@@ -139,31 +149,112 @@ public class IntegrateInfoActivity extends ToolbarBaseActivity {
     @Click
     @Background
     void okBtnClicked(View view) {
+        if (croppedProfileUri != null) {
+            String uriString = croppedProfileUri.toString();
+            String path = uriString.substring(uriString.indexOf(":") + 1);
+            String qiNiuKey = Config.PROFILE_PREFIX + AccountHelper.getIdentityInfo(this).get_id() + Config.URL_HYPHEN + System.currentTimeMillis() + Config.PROFILE_SUFFIX;
+
+            UploadManager uploadManager = new UploadManager();
+            new GetToken().setFileName(qiNiuKey).with(this).setCallback((e, result) -> {
+                if (e != null) {
+                    e.printStackTrace();
+                    showNotification(R.string.identityInfoUpdateFail);
+                } else if (result.getCode() != ErrorCode.SUCCESS) {
+                    showNotification(R.string.identityInfoUpdateFail);
+                    LogUtil.e(TAG, "get token failed: " + result.getCode());
+                } else {
+
+                    uploadManager.put(path, qiNiuKey, result.getData().getToken(),
+                            (s, responseInfo, jsonObject) -> {
+                                LogUtil.i(TAG, responseInfo.toString());
+                                if (responseInfo.isOK()) {
+                                    LogUtil.i(TAG, "profile upload ok");
+                                    updateInfo(qiNiuKey);
+                                } else {
+                                    LogUtil.e(TAG, "profile upload error");
+                                    showNotification(R.string.identityInfoUpdateFail);
+                                }
+                                hideProgress();
+                            },
+                            null
+                    );
+                }
+            });
+        } else updateInfo(null);
+
+
+    }
+
+    void updateInfo(@Nullable String qiNiuKey) {
         String nickname = String.valueOf(nickNameEditText.getText());
         if (TextUtils.isEmpty(nickname)) {
             shakeNickNameEditText();
             showNotification(R.string.integrateInfoNameEmpty);
         } else {
             showProgress();
-            ((IdentityInfo.Update) (new IdentityInfo.Update().with(this)))
-                    .setGender(gender[genderSelected])
-                    .setLocation(mProvince + mDistrict)
-                    .setNickname(nickname).setCallback((e, result) -> {
-                if (e != null) {
-                    e.printStackTrace();
-                    showNotification(R.string.integrateInfoUpdateFail);
-                } else if (result.getCode() == ErrorCode.SUCCESS) {
-                    AccountHelper.updateAccount(this, result.getData());
-                    showNotification(R.string.integrateInfoUpdateSuccess);
-                    setResult(RESULT_OK);
-                    this.finish();
-                } else {
-                    showNotification(R.string.integrateInfoUpdateFail);
-                }
-                hideProgress();
-            });
+            IdentityInfo.Update bu = ((IdentityInfo.Update) (new IdentityInfo.Update().with(this))).setGender(gender[genderSelected]);
+            if (qiNiuKey != null) bu = bu.setAvatarUrl(Config.getResourceUrl(this) + qiNiuKey);
+            bu.setLocation(mProvince + mDistrict).setNickname(nickname).setCallback(
+                    (e, result) -> {
+                        if (e != null) {
+                            e.printStackTrace();
+                            showNotification(R.string.integrateInfoUpdateFail);
+                        } else if (result.getCode() == ErrorCode.SUCCESS) {
+                            AccountHelper.updateAccount(this, result.getData());
+                            showNotification(R.string.integrateInfoUpdateSuccess);
+                            setResult(RESULT_OK);
+                            this.finish();
+                        } else {
+                            showNotification(R.string.integrateInfoUpdateFail);
+                        }
+                        hideProgress();
+                    });
         }
     }
 
+    @ViewById
+    ImageView profileImageView;
 
+    @Click
+    void profileImageViewClicked(View view) {
+        Crop.pickImage(this);
+    }
+
+    Uri croppedProfileUri;
+
+    @OnActivityResult(Crop.REQUEST_PICK)
+    @Background
+    void onPictureSelected(int resultCode, Intent data) {
+        if (resultCode == RESULT_OK && data != null && data.getData() != null) {
+            try {
+                Uri selectedImage = data.getData();
+                croppedProfileUri = Uri.fromFile(new File(getCacheDir(), "croppedProfile"));
+                new Crop(selectedImage).output(croppedProfileUri).asSquare().start(this);
+            } catch (Exception e) {
+                e.printStackTrace();
+                showNotification(R.string.identityInfoSelectProfileFail);
+            }
+        } else {
+            LogUtil.i(TAG, "RESULT_CANCELED");
+        }
+    }
+
+    @OnActivityResult(Crop.REQUEST_CROP)
+    @Background
+    void onPictureCropped(int resultCode, Intent data) {
+        if (resultCode == RESULT_OK) {
+            setProfileImage(croppedProfileUri);
+        } else croppedProfileUri = null;
+    }
+
+    @Background
+    void uploadProfile(Uri uri) {
+
+
+    }
+
+    @UiThread
+    void setProfileImage(Uri uri) {
+        Picasso.with(this).load(uri).into(profileImageView);
+    }
 }
