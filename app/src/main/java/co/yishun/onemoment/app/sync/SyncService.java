f1370295917e493d6fb12c97cea5c18a049138d1
Carlos <toxz@foxmail.com>
2015-04-05 18:52:24 +0800

    remove @EBean

--- app/src/main/java/co/yishun/onemoment/app/sync/SyncService.java
+++ app/src/main/java/co/yishun/onemoment/app/sync/SyncService.java
@@ -4,23 +4,36 @@ import android.app.Service;
 import android.content.Intent;
 import android.os.IBinder;
 import android.support.annotation.NonNull;
-import org.androidannotations.annotations.Bean;
-import org.androidannotations.annotations.EService;
+import co.yishun.onemoment.app.util.LogUtil;
 
 /**
  * This service is to listen the network status and sync in the background.
  * <p>
  * Created by Carlos on 2/7/15.
  */
-@EService
 public class SyncService extends Service {
     // Object to use as a thread-safe lock
-    @Bean(value = SyncAdapter.class)
-    static SyncAdapter mSyncAdapter = null;
+    private static final String TAG = LogUtil.makeTag(SyncService.class);
+    public static final Object mSyncAdapterLock = new Object();
+    private static SyncAdapter_ mSyncAdapter = null;
 
+    @Override
+    public void onCreate() {
+        /*
+         * Create the sync adapter as a singleton.
+         * Set the sync adapter as syncable
+         * Disallow parallel syncs
+         */
+        synchronized (mSyncAdapterLock) {
+            if (mSyncAdapter == null) {
+                mSyncAdapter = SyncAdapter_.getInstance_(getApplicationContext());
+            }
+        }
+    }
 
     @Override
     public IBinder onBind(@NonNull Intent intent) {
+        LogUtil.i(TAG, "SyncService onBind");
         return mSyncAdapter.getSyncAdapterBinder();
     }
 
