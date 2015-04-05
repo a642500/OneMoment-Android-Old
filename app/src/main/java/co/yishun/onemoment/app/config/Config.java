f8137cad0e77ba430e114a6985cb69fac5996a99
Carlos <toxz@foxmail.com>
2015-04-05 16:07:05 +0800

    add some new constants

--- app/src/main/java/co/yishun/onemoment/app/config/Config.java
+++ app/src/main/java/co/yishun/onemoment/app/config/Config.java
@@ -1,13 +1,19 @@
 package co.yishun.onemoment.app.config;
 
+import android.content.Context;
 import android.util.Pair;
 import co.yishun.onemoment.app.R;
+import co.yishun.onemoment.app.net.request.sync.GetDomain;
 
 /**
  * Created by Carlos on 2/15/15.
  */
 public class Config {
 
+    public static final String MIME_TYPE = "video/mp4";
+    public static final String VIDEO_FILE_SUFFIX = ".mp4";
+    public static final String URL_HYPHEN = "-";
+    public static final String LONG_VIDEO_PREFIX = "long";
     public static final String AES_KEY = "QJBBNfrwp2oN4ZBwT9qZ4MGObN8y56bEydJj48L8xVs=";
     public static final String URL_VERIFICATION_SEND_SMS = "/api/v2/send_verify_sms";
     public static final String URL_VERIFICATION_VERIFY = "/api/v2/verify_phone";
@@ -27,9 +33,16 @@ public class Config {
     private final static String URL_IDENTITY_INFO_GET = "/api/v2/account/";
     private final static String URL_IDENTITY_INFO_UPDATE = "/api/v2/update_account/";
     private final static String URL_CHECK_NICKNAME = "/api/v2/check_nickname";
+    private final static String URL_TOKEN = "/api/v2/upload_token";
+    private final static String URL_VIDEO_LIST = "/api/v2/videos/";
+    private final static String URL_RESOURCE_DOMAIN = "/api/v2/resource_domain";
     private static final Pair<Integer, Integer> mCameraSize = new Pair<>(480, 480);
     private static final int FFMPEG_RAW_ID = R.raw.ffmpeg;
     private final static String PRIVATE_KEY = "jfio2q3de0ajd0923i9faoik209q0r83u4rjew";
+
+    public static String getUrlDomain() {
+        return URL_MAIN + URL_RESOURCE_DOMAIN;
+    }
 
     public static String getUrlSignUpByPhone() {
         return URL_MAIN + URL_SIGN_UP_BY_PHONE;
@@ -72,6 +85,14 @@ public class Config {
         return URL_MAIN + URL_VERIFICATION_VERIFY;
     }
 
+    public static String getUrlToken() {
+        return URL_MAIN + URL_TOKEN;
+    }
+
+    public static String getUrlVideoList() {
+        return URL_MAIN + URL_VIDEO_LIST;
+    }
+
     public static String getPrivateKey() {
         return PRIVATE_KEY;
     }
@@ -82,5 +103,28 @@ public class Config {
 
     public static int getFfmpegRawId() {
         return FFMPEG_RAW_ID;
+    }
+
+    private static String mResourceDomain = null;
+
+    /**
+     * <strong>block thread</strong>
+     *
+     * @param context
+     * @return
+     */
+    private static String getResourceDomain(Context context) {
+        if (mResourceDomain == null) {
+            new GetDomain().setCallback((e, result) -> {
+                if (e != null) e.printStackTrace();
+                else mResourceDomain = result.getData().getDomain();
+            });
+        }
+        return mResourceDomain;
+    }
+
+    public static String getResourceUrl(Context context) {
+        String main = getResourceDomain(context);
+        return (main.endsWith("/") ? main : main + "/");
     }
 }
