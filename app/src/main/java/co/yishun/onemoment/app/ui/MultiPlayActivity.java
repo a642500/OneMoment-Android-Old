a59b435cf0dfb5abe464e19dfd50aa4ffbd8cba2
Carlos <toxz@foxmail.com>
2015-04-07 23:05:03 +0800

    make multiPlay act can seamless play

--- app/src/main/java/co/yishun/onemoment/app/ui/MultiPlayActivity.java
+++ app/src/main/java/co/yishun/onemoment/app/ui/MultiPlayActivity.java
@@ -13,7 +13,9 @@ import co.yishun.onemoment.app.data.Moment;
 import com.squareup.picasso.Picasso;
 import org.androidannotations.annotations.*;
 
+import java.util.ArrayDeque;
 import java.util.List;
+import java.util.Queue;
 
 /**
  * Created by Carlos on 2015/4/5.
@@ -22,8 +24,6 @@ import java.util.List;
 public class MultiPlayActivity extends ToolbarBaseActivity {
     @ViewById
     VideoView videoView;
-    @ViewById
-    VideoView videoViewAnother;
     @ViewById
     ImageView thumbImageView;
     @ViewById
@@ -40,11 +40,10 @@ public class MultiPlayActivity extends ToolbarBaseActivity {
     @Extra
     List<Moment> moments;
 
-    int point = 0;
-    boolean pendingEnd = false;
+    Queue<Moment> toPlayMoments;
 
     @AfterViews
-    void initVideo() {
+    void setSquare() {
         ViewTreeObserver viewTreeObserver = recordSurfaceParent.getViewTreeObserver();
         if (viewTreeObserver.isAlive()) {
             viewTreeObserver.addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
@@ -57,69 +56,43 @@ public class MultiPlayActivity extends ToolbarBaseActivity {
                 }
             });
         }
+    }
 
+    private void queueMoments() {
+        toPlayMoments.clear();
+        toPlayMoments.addAll(moments);
+    }
 
+    @AfterViews
+    void initVideo() {
+        toPlayMoments = new ArrayDeque<>(moments.size());
+        queueMoments();
         if (BuildConfig.DEBUG && !(moments.size() >= 2))
             throw new RuntimeException("moment's length is 1 or 0, use PlayActivity");
 
         Picasso.with(this).load("file://" + moments.get(0).getLargeThumbPath()).into(thumbImageView);
-        videoView.setVideoPath(moments.get(point).getPath());
-        point++;
+        //TODO add cover video
 
         videoView.setOnCompletionListener(mp -> {
             mp.reset();
-            if (pendingEnd) {
-                end();
-                return;
+            Moment moment = toPlayMoments.poll();
+            if (moment != null) {
+                videoView.setVideoPath(moment.getPath());
+                videoView.start();
+            } else {
+                playBtn.setVisibility(View.VISIBLE);
+                thumbImageView.setVisibility(View.VISIBLE);
+                queueMoments();
             }
-
-            videoViewAnother.start();
-            videoViewAnother.setVisibility(View.VISIBLE);
-            videoView.setVisibility(View.INVISIBLE);
-            if (point < moments.size()) {
-                videoView.setVideoPath(moments.get(point).getPath());
-                point++;
-            } else pendingEnd = true;
-        });
-        videoViewAnother.setOnCompletionListener(mp -> {
-            mp.reset();
-            if (pendingEnd) {
-                end();
-                return;
-            }
-
-            videoView.start();
-            videoViewAnother.setVisibility(View.INVISIBLE);
-            videoView.setVisibility(View.VISIBLE);
-            if (point < moments.size()) {
-                videoViewAnother.setVideoPath(moments.get(point).getPath());
-                point++;
-            } else pendingEnd = true;
         });
     }
-
-    private void end() {
-        thumbImageView.setVisibility(View.VISIBLE);
-        playBtn.setVisibility(View.VISIBLE);
-        point = 0;
-    }
-
-//    @Click
-//    void thumbImageViewClicked(View view) {
-//        view.setVisibility(View.GONE);
-//        videoView.start();
-//    }
 
     @Click
     void playBtnClicked(View view) {
+        videoView.setVideoPath(toPlayMoments.poll().getPath());
         view.setVisibility(View.GONE);
-        thumbImageView.setVisibility(View.GONE);
-
-        videoViewAnother.setVisibility(View.INVISIBLE);
-        videoView.setVisibility(View.VISIBLE);
         videoView.start();
-        videoViewAnother.setVideoPath(moments.get(point).getPath());
-        point++;
+        thumbImageView.setVisibility(View.GONE);
     }
 
     @Click
