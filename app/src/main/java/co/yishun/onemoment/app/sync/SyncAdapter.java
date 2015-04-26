9c73c8cc3b6cc5519a8361b85a32ab3a27091433
Carlos <toxz@foxmail.com>
2015-04-27 01:12:44 +0800

    add sync progress update and sync done broadcast

--- app/src/main/java/co/yishun/onemoment/app/sync/SyncAdapter.java
+++ app/src/main/java/co/yishun/onemoment/app/sync/SyncAdapter.java
@@ -45,6 +45,9 @@ public class SyncAdapter extends AbstractThreadedSyncAdapter {
         mContentResolver = context.getContentResolver();
     }
 
+    /**
+     * To execute sync. When sync end, it will call {@link #syncDone(boolean)} to send broadcast notify sync process ending. And in syncing process, it calls {@link #syncUpdate(UpdateType, boolean, int, int, int)} to broadcast progress.
+     */
     @Override
     public void onPerformSync(Account account, Bundle extras, String authority, ContentProviderClient provider, SyncResult syncResult) {
         LogUtil.i(TAG, "onPerformSync, account: " + account.name + ", Bundle: " + extras);
@@ -53,9 +56,13 @@ public class SyncAdapter extends AbstractThreadedSyncAdapter {
         new GetVideoList().with(getContext()).setCallback((e, result) -> {
             if (e != null) {
                 e.printStackTrace();
+                syncDone(false);
             } else if (result.getCode() == ErrorCode.SUCCESS) {
                 sync(toMap(result.getDatas()));
-            } else LogUtil.e(TAG, "get video list failed: " + result.getCode());
+            } else {
+                LogUtil.e(TAG, "get video list failed: " + result.getCode());
+                syncDone(false);
+            }
         });
 
     }
@@ -63,54 +70,113 @@ public class SyncAdapter extends AbstractThreadedSyncAdapter {
     @OrmLiteDao(helper = MomentDatabaseHelper.class, model = Moment.class)
     Dao<Moment, Integer> dao;
 
+    @Override public void onSyncCanceled() {
+        //TODO how to handle cancel
+        super.onSyncCanceled();
+    }
 
+    /**
+     * compare local video with server's, and determine whether upload or download.
+     * <p>
+     * TODO Refactor: just determine which action to take, and commit the action task to a queue
+     *
+     * @param videosOnServer map of server's videos
+     */
     private void sync(Map<Integer, Data> videosOnServer) {
-        LogUtil.i(TAG, "video got, start sync");
-        for (Moment moment : dao) {
-            Integer key = Integer.parseInt(moment.getTime());
-            Data video = videosOnServer.get(key);
-            LogUtil.v(TAG, "sync iter: " + moment.toString());
-            if (video != null) {
-                LogUtil.v(TAG, "on server: " + video.toString());
-                //if server has today moment
-                if (video.getTimeStamp() > moment.getTimeStamp()) {
-                    //if server is newer, download and delete older
-                    LogUtil.v(TAG, "download: " + video.toString());
-                    downloadVideo(video, moment);
-                } else if (video.getTimeStamp() < moment.getTimeStamp()) {
-                    //if local is newer, upload and delete older
-                    LogUtil.v(TAG, "upload: " + video.toString());
-                    uploadMoment(moment, video);
-                } else {
-                    LogUtil.v(TAG, "same, do nothing");
-                }
-                //the video sync ok, remove
-                videosOnServer.remove(key);
-            } else uploadMoment(moment, null);//server not have today moment, upload
+        try {
+            LogUtil.i(TAG, "video got, start sync");
+            for (Moment moment : dao) {
+                Integer key = Integer.parseInt(moment.getTime());
+                Data video = videosOnServer.get(key);
+                LogUtil.v(TAG, "sync iter: " + moment.toString());
+                if (video != null) {
+                    LogUtil.v(TAG, "on server: " + video.toString());
+                    //if server has today moment
+                    if (video.getTimeStamp() > moment.getTimeStamp()) {
+                        //if server is newer, download and delete older
+                        LogUtil.v(TAG, "download: " + video.toString());
+                        downloadVideo(video, moment);
+                    } else if (video.getTimeStamp() < moment.getTimeStamp()) {
+                        //if local is newer, upload and delete older
+                        LogUtil.v(TAG, "upload: " + video.toString());
+                        uploadMoment(moment, video);
+                    } else {
+                        LogUtil.v(TAG, "same, do nothing");
+                    }
+                    //the video sync ok, remove
+                    videosOnServer.remove(key);
+                } else uploadMoment(moment, null);//server not have today moment, upload
+            }
+            for (Data data : videosOnServer.values())
+                downloadVideo(data, null);//other unhandled video mean they need download
+            syncDone(true);
+        } catch (Exception e) {
+            //catch all to avoid crash background
+            e.printStackTrace();
+            syncDone(false);
         }
-        for (Data data : videosOnServer.values())
-            downloadVideo(data, null);//other unhandled video mean they need download
-        syncDone();
     }
 
     /**
      * send broadcast to notify finishing syncing
      */
-    private void syncDone() {
-        Intent intent = new Intent(SYNC_BROADCAST);
-        intent.putExtra(SYNC_BROADCAST_EXTRA_IS_CHANGED, isChange);
-        LogUtil.i(TAG, "sync done, send a broadcast. isChange: " + isChange);
+    private void syncDone(boolean isSuccess) {
+        Intent intent = new Intent(SYNC_BROADCAST_DONE);
+        intent.putExtra(SYNC_BROADCAST_EXTRA_IS_UPLOAD_CHANGED, isUploadChanged);
+        intent.putExtra(SYNC_BROADCAST_EXTRA_IS_DOWNLOAD_CHANGED, isDownloadChanged);
+        intent.putExtra(SYNC_BROADCAST_EXTRA_IS_SUCCESS, isSuccess);
+        LogUtil.i(TAG, "sync done, send a broadcast. isUploadChanged: " + isUploadChanged + ", isDownloadChanged: " + isDownloadChanged + ", isSuccess " + isSuccess);
         getContext().sendBroadcast(intent);
+        isUploadChanged = false;
+        isDownloadChanged = false;
+    }
+
+    /**
+     * send broadcast to notify syncing progress update.
+     * <p>
+     * progress is from 0 to 100.
+     * <p>
+     * {@link #PROGRESS_NOT_AVAILABLE} means no progress data available.
+     * {@link #PROGRESS_ERROR} means error occurred.
+     */
+    private void syncUpdate(UpdateType type, int thisProgress, int thisTypeProgress, int allProgress) {
+        Intent intent = new Intent(type.getAction());
+        intent.putExtra(SYNC_BROADCAST_EXTRA_THIS_PROGRESS, thisProgress);
+        intent.putExtra(SYNC_BROADCAST_EXTRA_TYPE_PROGRESS, thisTypeProgress);
+        intent.putExtra(SYNC_BROADCAST_EXTRA_ALL_PROGRESS, allProgress);
+        LogUtil.i(TAG, "sync update progress, send a broadcast. type: " + type.name() + ", this progress: " + thisProgress + ", type progress: " + thisTypeProgress + ", all progress: " + allProgress);
+        getContext().sendBroadcast(intent);
+    }
+
+    enum UpdateType {
+        UPLOAD {
+            @Override String getAction() { return SYNC_BROADCAST_UPDATE_UPLOAD; }
+        },
+        DOWNLOAD {
+            @Override String getAction() { return SYNC_BROADCAST_UPDATE_DOWNLOAD; }
+        };
+
+        abstract String getAction();
     }
 
     /**
      * whether local data update while sync. if true, need to notify some ui update
      */
-    boolean isChange = false;
+    boolean isUploadChanged = false;
+    boolean isDownloadChanged = false;
 
-    public static final String SYNC_BROADCAST = "co.yishun.onemoment.app.sync.done";
-    public static final String SYNC_BROADCAST_EXTRA_IS_CHANGED = "is_changed";
+    public static final String SYNC_BROADCAST_DONE = "co.yishun.onemoment.app.sync.done";
+    public static final String SYNC_BROADCAST_UPDATE_UPLOAD = "co.yishun.onemoment.app.sync.update.upload";
+    public static final String SYNC_BROADCAST_UPDATE_DOWNLOAD = "co.yishun.onemoment.app.sync.update.download";
+    public static final String SYNC_BROADCAST_EXTRA_IS_UPLOAD_CHANGED = "is_upload_changed";
+    public static final String SYNC_BROADCAST_EXTRA_IS_DOWNLOAD_CHANGED = "is_download_changed";
+    public static final String SYNC_BROADCAST_EXTRA_IS_SUCCESS = "is_success";
+    public static final String SYNC_BROADCAST_EXTRA_THIS_PROGRESS = "int_this_progress";
+    public static final String SYNC_BROADCAST_EXTRA_TYPE_PROGRESS = "int_type_progress";
+    public static final String SYNC_BROADCAST_EXTRA_ALL_PROGRESS = "int_all_progress";
 
+    public static final int PROGRESS_NOT_AVAILABLE = -1;
+    public static final int PROGRESS_ERROR = -2;
 
     private UploadManager mUploadManager;
 
@@ -124,6 +190,7 @@ public class SyncAdapter extends AbstractThreadedSyncAdapter {
     private void uploadMoment(Moment moment, Data videoToDelete) {
         LogUtil.i(TAG, "upload a moment: " + moment.getPath());
         String qiNiuKey = getQiniuVideoFileName(moment);
+        syncUpdate(UpdateType.UPLOAD, 0, PROGRESS_NOT_AVAILABLE, PROGRESS_NOT_AVAILABLE);
         new GetToken().setFileName(qiNiuKey).with(getContext()).setCallback((e, result) -> {
             if (e != null) {
                 e.printStackTrace();
@@ -134,8 +201,13 @@ public class SyncAdapter extends AbstractThreadedSyncAdapter {
                         result.getData().getToken(),
                         (s, responseInfo, jsonObject) -> {
                             LogUtil.i(TAG, responseInfo.toString());
-                            if (videoToDelete != null) deleteVideo(videoToDelete);
-                            LogUtil.i(TAG, "a moment upload ok: " + moment.getPath());
+                            if (responseInfo.isOK()) {
+                                if (videoToDelete != null) deleteVideo(videoToDelete);
+                                LogUtil.i(TAG, "a moment upload ok: " + moment.getPath());
+                                isUploadChanged = true;
+                                syncUpdate(UpdateType.UPLOAD, 100, PROGRESS_NOT_AVAILABLE, PROGRESS_NOT_AVAILABLE);
+                            } else
+                                syncUpdate(UpdateType.UPLOAD, PROGRESS_ERROR, PROGRESS_NOT_AVAILABLE, PROGRESS_NOT_AVAILABLE);
                         },
                         new UploadOptions(null, Config.MIME_TYPE, true, null, null)
                 );
@@ -184,7 +256,7 @@ public class SyncAdapter extends AbstractThreadedSyncAdapter {
 //            Request request = new Request.Builder().url(Config.getResourceUrl(getContext()) + aVideoOnServer.getQiuniuKey()).get().build();
 //            Response response = client.newCall(request).execute();
 //            response.body().
-
+        syncUpdate(UpdateType.DOWNLOAD, 0, PROGRESS_NOT_AVAILABLE, PROGRESS_NOT_AVAILABLE);
         Ion.with(getContext()).load(Config.getResourceUrl(getContext()) + aVideoOnServer.getQiuniuKey())
                 .write(fileTemp).setCallback((e, result) -> {
             try {
@@ -197,14 +269,17 @@ public class SyncAdapter extends AbstractThreadedSyncAdapter {
                 }
                 if (!fileTemp.renameTo(file)) {
                     LogUtil.e(TAG, "rename synced to local failed: " + fileTemp.getPath());
+                    syncUpdate(UpdateType.DOWNLOAD, PROGRESS_ERROR, PROGRESS_NOT_AVAILABLE, PROGRESS_NOT_AVAILABLE);
                 } else {
                     String pathToThumb = CameraHelper.createThumbImage(getContext(), file.getPath());
                     String pathToLargeThumb = CameraHelper.createLargeThumbImage(getContext(), file.getPath());
                     dao.create(Moment.from(aVideoOnServer, file.getPath(), pathToThumb, pathToLargeThumb));
                     LogUtil.i(TAG, "a video download ok: " + aVideoOnServer.getQiuniuKey());
-                    isChange = true;
+                    isDownloadChanged = true;
+                    syncUpdate(UpdateType.DOWNLOAD, 100, PROGRESS_NOT_AVAILABLE, PROGRESS_NOT_AVAILABLE);
                 }
             } catch (Exception e1) {
+                syncUpdate(UpdateType.DOWNLOAD, PROGRESS_ERROR, PROGRESS_NOT_AVAILABLE, PROGRESS_NOT_AVAILABLE);
                 e1.printStackTrace();
             }
         });
