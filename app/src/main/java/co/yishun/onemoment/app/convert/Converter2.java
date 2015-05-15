df63a4df88b0ecadd3328b75d1152ace23dec357
a642500 <toxz@foxmail.com>
2015-05-16 01:36:52 +0800

    force use armeabi-v7a for all cpu

--- app/src/main/java/co/yishun/onemoment/app/convert/Converter2.java
+++ app/src/main/java/co/yishun/onemoment/app/convert/Converter2.java
@@ -1,12 +1,15 @@
 package co.yishun.onemoment.app.convert;
 
 import android.content.Context;
+import android.os.AsyncTask;
 import co.yishun.onemoment.app.util.LogUtil;
 import com.github.hiteshsondhi88.libffmpeg.FFmpeg;
 import com.github.hiteshsondhi88.libffmpeg.FFmpegExecuteResponseHandler;
 import com.github.hiteshsondhi88.libffmpeg.FFmpegLoadBinaryResponseHandler;
 import com.github.hiteshsondhi88.libffmpeg.exceptions.FFmpegCommandAlreadyRunningException;
-import com.github.hiteshsondhi88.libffmpeg.exceptions.FFmpegNotSupportedException;
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
 
 /**
  * Created by Carlos on 3/7/15.
@@ -21,7 +24,7 @@ class Converter2 extends Converter {
         LogUtil.i(TAG, "Converter2 get instance time: " + System.currentTimeMillis());
         mFFmpeg = FFmpeg.getInstance(context);
         try {
-            mFFmpeg.loadBinary(new FFmpegLoadBinaryResponseHandler() {
+            loadLibraryProxy(context, mFFmpeg, new FFmpegLoadBinaryResponseHandler() {
                 @Override
                 public void onFailure() {
                     LogUtil.e(TAG, "load error");
@@ -42,11 +45,19 @@ class Converter2 extends Converter {
                     LogUtil.i(TAG, "load finish");
                 }
             });
-        } catch (FFmpegNotSupportedException e) {
+        } catch (Exception e) {
             e.printStackTrace();
+            LogUtil.e(TAG, "proxy load exception", e);
         }
     }
 
+    // force load armeabi-v7a, because others are removed by gradle
+    private static void loadLibraryProxy(Context context, FFmpeg ffmpeg, FFmpegLoadBinaryResponseHandler handler) throws NoSuchFieldException, IllegalAccessException, NoSuchMethodException, InvocationTargetException, InstantiationException, ClassNotFoundException {
+        Constructor constructor = Class.forName("com.github.hiteshsondhi88.libffmpeg.FFmpegLoadLibraryAsyncTask").getDeclaredConstructors()[0];
+        constructor.setAccessible(true);
+        AsyncTask<Void, Void, Boolean> task = (AsyncTask<Void, Void, Boolean>) constructor.newInstance(context, "armeabi-v7a", handler);
+        task.execute();
+    }
 
     @Override
     public Converter setHandler(FFmpegExecuteResponseHandler handler) {
@@ -54,8 +65,7 @@ class Converter2 extends Converter {
         return this;
     }
 
-    @Override
-    void run() {
+    @Override void run() {
         try {
             String cmd = mCommand.toString();
             LogUtil.i(TAG, "cmd: " + cmd);
