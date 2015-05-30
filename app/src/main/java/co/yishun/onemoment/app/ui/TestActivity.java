b715a08c76f90f2cd6e4e70de84e30010808996a
a642500 <toxz@foxmail.com>
2015-05-31 00:25:16 +0800

    rename old inner test flavor to marketGroupOrPersonal create inner test flavor for inner test

---
+++ app/src/main/java/co/yishun/onemoment/app/ui/TestActivity.java
@@ -0,0 +1,15 @@
+package co.yishun.onemoment.app.ui;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import co.yishun.onemoment.app.R;
+
+
+public class TestActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_test);
+    }
+}
